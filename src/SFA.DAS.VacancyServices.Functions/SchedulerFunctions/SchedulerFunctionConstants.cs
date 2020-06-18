6d9290460db7660b788d4d5d655e6c285e718c36
Paul Howes <paul.howes@cognetics.net>
2020-06-18 10:23:46 +0100

    Removed constant

--- src/SFA.DAS.VacancyServices.Functions/SchedulerFunctions/SchedulerFunctionConstants.cs
+++ src/SFA.DAS.VacancyServices.Functions/SchedulerFunctions/SchedulerFunctionConstants.cs
@@ -10,7 +10,6 @@ namespace SFA.DAS.VacancyServices.Functions.SchedulerFunctions
             public const string DailyMetricsSchedulerQueueName = "dailymetricsscheduler";
             public const string GeoCodeSchedulerQueueName = "geocodescheduler";
             public const string HouseKeepingSchedulerQueueName = "housekeepingscheduler";
-            public const string MonitorSchedulerQueueName = "monitorscheduler";
             public const string ReferenceDataSchedulerQueueName = "referencedatascheduler";
             public const string SavedSearchSchedulerQueueName = "savedsearchscheduler";
             public const string VacancyIndexSchedulerQueueName = "vacancyindexscheduler";
